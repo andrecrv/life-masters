d1c83d3dcd92224e1a1e3c504b1f364f646e074a
andrc-dev <andrecraveiro95@gmail.com>
2023-11-23 20:26:41 +0000

    setup: add project files to git

---
+++ src/main/java/com/demomasters/lifemasters/Playground.java
@@ -0,0 +1,13 @@
+package com.demomasters.lifemasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Playground {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Playground.class, args);
+    }
+
+}
---
+++ src/test/java/com/demomasters/lifemasters/LifemastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demomasters.lifemasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifemastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
