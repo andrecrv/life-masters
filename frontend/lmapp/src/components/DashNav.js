c86d4de848843a3431985a943a38733f3201616a
andrc-dev <andrecraveiro95@gmail.com>
2023-12-03 17:46:24 +0000

    feature: Add new menu buttons to menu list and introduce user profile component
    Expand the menu list with new buttons for enhanced functionality.
    Introduce a user profile component featuring an icon and additional details such as username, title and level displayed as text.

--- frontend/lmapp/src/components/DashNav.js
+++ frontend/lmapp/src/components/DashNav.js
@@ -5,22 +5,33 @@ import React from 'react';
 //import useAuth from '../hooks/useAuth';
 import '../styles/dashNav.css';
 import Menu from './Menu';
+import UserProfile from './UserProfile';
 
 const DashNav = () => {
 
+    const userDao = {
+        icon: '/resources/icons/icon8.jpg',
+        username: 'user_not_found',
+        title: 'Unfoundable',
+        lvl: '+9999',
+    }
+
     const buttonList = [
+        { text: 'Overview' },
         { text: 'Todo List' },
-        { text: 'Mastery' },
+        { text: 'Goals' },
+        { text: 'Challenges' },
+        { text: 'Knowledge' },
+        { text: 'Guide' },
+        { text: 'Friends' },
+        { text: 'Achievements' },
         { text: 'Settings' },
         { text: 'Logout' },
-        
     ];
 
     return (
         <div className="nav">
-            <div className="nav-head">
-                User Info
-            </div>
+            <UserProfile user={userDao} />
             <Menu buttons={buttonList} />
         </div>
     );
