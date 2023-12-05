13d9ae6ff2a47d1c047f2c9ea4e8151921c4af14
andrc-dev <andrecraveiro95@gmail.com>
2023-12-05 19:04:40 +0000

    feature: Add a ListItem component to use as a task element

---
+++ frontend/lmapp/src/components/ListItem/ListItem.js
@@ -0,0 +1,15 @@
+// ListItem.js
+
+import React from 'react';
+import './list-item.css';
+
+const ListItem = ({ itemData }) => {
+
+    return (
+        <div className="item-container">
+            Item
+        </div>
+    );
+};
+
+export default ListItem;
