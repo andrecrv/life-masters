e2babfea3ce3b7b68db2c990aa6a5177dfae414c
andrc-dev <andrecraveiro95@gmail.com>
2024-12-03 20:14:49 +0000

    refactor: redesign ListItem component with updated functionality
    Redesigned the ListItem component to improve usability and maintainability.
    Added icons to buttons for better clarity and replaced the complete toggle element with a new one.
    Fixed issues with flexbox alignment and adjusted dimensions for a more consistent layout.

--- frontend/lmapp/src/components/ListItem/ListItem.js
+++ frontend/lmapp/src/components/ListItem/ListItem.js
@@ -1,7 +1,14 @@
 // ListItem.js
 
 import { useState } from 'react';
+import ActionButton from '../common/ActionButton';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+
 import './list-item.css';
+
 
 const ListItem = ({ item, updateItem, editItem, deleteItem }) => {
 
@@ -12,12 +19,13 @@ const ListItem = ({ item, updateItem, editItem, deleteItem }) => {
     editable ? setEditable(false) : setEditable(true);
   }
 
-  const handleEdit = (e) => {
-    if (e.target.name === 'save') {
-      editItem(item.id, text);
-    } else if (e.target.name === 'cancel') {
-      setText(item.text);
-    }
+  const saveEdit = () => {
+    editItem(item.id, text);
+    editMode();
+  }
+
+  const cancelEdit = () => {
+    setText(item.text);
     editMode();
   }
 
@@ -27,33 +35,76 @@ const ListItem = ({ item, updateItem, editItem, deleteItem }) => {
 
   return (
     <div className="item-container">
-      <div className="check-btn-container">
-        <button
-          className={item.complete ? "check-btn active" : "check-btn"}
+      <div className={item.complete ? "checkbox-container-active" : "checkbox-container"}>
+        {/*<input
           type="checkbox"
+          className="checkbox"
+          /*className={item.complete ? "check-btn active" : "check-btn"}*/
+          /*onClick={() => updateItem(item.id)}
+        />*/}
+        <button
+          className={item.complete ? "checkbox active" : "checkbox"}
           onClick={() => updateItem(item.id)}
         />
       </div>
-      <div className="text-container">
+      <div className="item-input-container">
         {editable ?
-          <input type='text' name='input-edit' value={text} onChange={changeText} />
+          <input
+            type='text'
+            name='item-input'
+            className='item-input'
+            value={text}
+            onChange={changeText}
+          />
           :
-          item.text
+          <div className="item-text">
+            {item.text}
+          </div>
         }
       </div>
       <div className="edit-btn-container">
         {/* If item is editable (true), then render Save button, else render Edit button */}
         {editable ?
-          <button name='save' className="btn save-btn" type="button" onClick={handleEdit} >Save</button>
+          <button name='save' className="save-btn" type="button" onClick={saveEdit} >
+            <ActionButton
+              name='save'
+              icon={CheckIcon}
+              size="1em"
+              backgroundColor="none"
+              color="white"
+            />
+          </button>
           :
-          <button name='edit' className="btn edit-btn" type="button" onClick={editMode} >Edit</button>
+          <button name='edit' className="edit-btn" type="button" onClick={editMode} >
+            <ActionButton
+              icon={EditIcon}
+              size="1em"
+              backgroundColor="none"
+              color="white"
+            />
+          </button>
         }
       </div>
       <div className="delete-btn-container">
         {editable ?
-          <button name='cancel' className="btn cancel-btn" type="button" onClick={handleEdit} >Cancel</button>
+          <button name='cancel' className="cancel-btn" type="button" onClick={cancelEdit} >
+            <ActionButton
+              name='cancel'
+              icon={CloseIcon}
+              size="1em"
+              backgroundColor="none"
+              color="white"
+            />
+          </button>
           :
-          <button name='delete' className="btn del-btn" type="button" onClick={() => deleteItem(item.id)} >Delete</button>
+          <button name='delete' className="del-btn" type="button" onClick={() => deleteItem(item.id)} >
+            <ActionButton
+              icon={DeleteIcon}
+              size="1em"
+              backgroundColor="none"
+              color="white"
+            />
+          </button>
         }
       </div>
     </div>
